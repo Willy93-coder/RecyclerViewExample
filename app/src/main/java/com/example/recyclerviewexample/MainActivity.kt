89ab3c03d136f89ff974b24d9795fc42b5174816
Willy <guillecg13@gmail.com>
2023-06-17 19:07:29 +0200

    Added: recycler view

--- app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.recyclerviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recyclerviewexample.adapter.SimpsonCharacterAdapter
 import com.example.recyclerviewexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initRecyclerView()
+    }
+
+    // Función para iniciar el recyclerView
+    private fun initRecyclerView() {
+        val manager = LinearLayoutManager(this)
+        binding.recyclerSimpsonCharacter.layoutManager = manager
+        binding.recyclerSimpsonCharacter.adapter =
+            SimpsonCharacterAdapter(
+                SimpsonCharacterProvider.simpsonCharacterList
+            ) { simpsonCharacter -> onItemSelected(simpsonCharacter) }
+    }
+
+    private fun onItemSelected(simpsonCharacter: SimpsonCharacter) {
+        Toast.makeText(this, simpsonCharacter.name, Toast.LENGTH_SHORT).show()
     }
 }
